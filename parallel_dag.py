fc8ee5848d3f4045f5d76d356cb9a7b91ed7eba2
Marc LAMBERTI <marclamberti.ml@gmail.com>
2019-11-30 09:06:20 +0100

    update

--- parallel_dag.py
+++ parallel_dag.py
@@ -16,7 +16,7 @@ def process(p1):
 with DAG(dag_id='parallel_dag', schedule_interval='0 0 * * *', default_args=default_args, catchup=False) as dag:
     
     # Tasks dynamically generated 
-    tasks = [BashOperator(task_id='task_{0}'.format(t), bash_command='sleep 5'.format(t)) for t in range(1, 4)]
+    tasks = [BashOperator(task_id='task_{0}'.format(t), bash_command='sleep 60'.format(t)) for t in range(1, 4)]
 
     task_4 = PythonOperator(task_id='task_4', python_callable=process, op_args=['my super parameter'])
 
