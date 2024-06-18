4f366591f2d96a181eb3f9ef86a4a33c73ee88a4
Anton Kuiper <anton.kuiper@gmail.com>
2024-06-18 23:00:00 +0200

    van 7 naar 4 concurrent

--- DynamicTPC_Bucket2Raw.py
+++ DynamicTPC_Bucket2Raw.py
@@ -45,7 +45,7 @@ dag = DAG(f'Dynamic_{MDP_APPLICATION}_Bucket2Raw',
           description=f'Dynamically generated {MDP_APPLICATION} load from bucket to Raw DAG',
           schedule_interval='0 12 * 1 *',
           start_date=datetime(2024, 5, 22),
-          concurrency=7,
+          concurrency=4,
           catchup=False)
 
 # Fetch the data from the database
