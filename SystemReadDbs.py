0f3ecc31f80d5e599364197a70dc5077952d424b
Anton Kuiper <anton.kuiper@gmail.com>
2024-06-19 13:35:56 +0200

    || > +

--- SystemReadDbs.py
+++ SystemReadDbs.py
@@ -8,7 +8,7 @@ from kubernetes.client import models as k8s
 # Define the mdp_application value only once
 MDP_APPLICATION = 'tpc.dbs'  # Change this value as needed
 MDP_Path = 'dbschema/'
-mdp_path_app = MDP_Path || MDP_APPLICATION
+mdp_path_app = MDP_Path + MDP_APPLICATION
 
 
 default_args = {
